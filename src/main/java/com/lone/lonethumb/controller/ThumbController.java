ba6144753d0a801b439b8d441c8805b246ee1d98
lonelyli0719 <794156416@qq.com>
2025-07-26 21:05:10 +0800

    高并发点赞系统-集成Prometheus,实现系统指标的可观测性

--- src/main/java/com/lone/lonethumb/controller/ThumbController.java
+++ src/main/java/com/lone/lonethumb/controller/ThumbController.java
@@ -4,6 +4,8 @@ import com.lone.lonethumb.common.BaseResponse;
 import com.lone.lonethumb.common.ResultUtils;
 import com.lone.lonethumb.model.dto.DoThumbRequest;
 import com.lone.lonethumb.service.ThumbService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +18,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class ThumbController {  
     @Resource
     private ThumbService thumbService;
-  
+
+    /**
+     * 集成计数器指标
+     */
+    private final Counter successCounter;
+    private final Counter failureCounter;
+
+    public ThumbController(MeterRegistry registry) {
+        this.successCounter = Counter.builder("thumb.success.count")
+                .description("Total successful thumb")
+                .register(registry);
+        this.failureCounter = Counter.builder("thumb.failure.count")
+                .description("Total failed thumb")
+                .register(registry);
+    }
+
     @PostMapping("/do")
     public BaseResponse<Boolean> doThumb(@RequestBody DoThumbRequest doThumbRequest, HttpServletRequest request) {
-        Boolean success = thumbService.doThumb(doThumbRequest, request);  
+        Boolean success;
+        try {
+            success = thumbService.doThumb(doThumbRequest, request);
+            if (success) {
+                successCounter.increment();
+            } else {
+                failureCounter.increment();
+            }
+        } catch (Exception e) {
+            failureCounter.increment();
+            throw e;
+        }
         return ResultUtils.success(success);
     }
 
